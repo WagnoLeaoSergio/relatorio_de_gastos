a1234dd48dedbca2fe100bd088cc4484efa3b5be
Wagno <wagnoleao@gmail.com>
2020-12-10 21:14:01 -0300

    setup.sh atualizado

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"wagnoleao@gmail.com\"\n\
+email = \"your@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
